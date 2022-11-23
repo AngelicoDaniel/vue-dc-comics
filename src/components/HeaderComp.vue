35284f3cc0ca20effc491cf95dc79a80e20547d3
Daniel <angelicodaniel93@gmail.com>
2022-11-23 14:30:18 +0100

    inizio header

---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <img src="../assets/img/dc-logo.png" alt="DC-logo">
+    <ul>
+      <li>CHARACTERS</li>
+      <li>COMICS</li>
+      <li>MOVIES</li>
+      <li>TV</li>
+      <li>GAMES</li>
+      <li>COLLECTIBLES</li>
+      <li>VIDEOS</li>
+      <li>FANS</li>
+      <li>NEWS</li>
+      <li>SHOP</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderComp'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
